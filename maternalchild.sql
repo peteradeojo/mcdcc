f373b9dedcc337e932c34eae4b9b245454ee1d89
peteradeojo <peteradeojo@outlook.com>
2021-08-10 10:49:01 +0100

    database config

--- maternalchild.sql
+++ maternalchild.sql
@@ -1,86 +1,110 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
+-- MariaDB dump 10.19  Distrib 10.4.20-MariaDB, for Win64 (AMD64)
 --
--- Host: 127.0.0.1
--- Generation Time: Aug 09, 2021 at 11:49 PM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 8.0.9
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: maternalchild
+-- ------------------------------------------------------
+-- Server version	10.4.20-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Database: `maternalchild`
+-- Table structure for table `cards`
 --
 
--- --------------------------------------------------------
-
---
--- Table structure for table `patients`
---
-
-CREATE TABLE `patients` (
-  `id` int(11) NOT NULL COMMENT 'primary key',
-  `cardnumber` varchar(11) NOT NULL COMMENT 'card number',
-  `firstname` varchar(20) NOT NULL,
-  `lastname` varchar(20) NOT NULL,
-  `phone` varchar(20) DEFAULT NULL COMMENT 'phone number',
+DROP TABLE IF EXISTS `cards`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cards` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'primary key',
   `create_time` datetime DEFAULT current_timestamp() COMMENT 'create time',
-  `update_time` datetime DEFAULT NULL COMMENT 'update time'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
+  `update_time` datetime DEFAULT NULL COMMENT 'update time',
+  `code` varchar(3) NOT NULL COMMENT 'card unique id',
+  `title` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `code` (`code`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `patient_details`
 --
 
+DROP TABLE IF EXISTS `patient_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `patient_details` (
-  `id` int(11) NOT NULL COMMENT 'primary key',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'primary key',
   `create_time` datetime DEFAULT current_timestamp() COMMENT 'create time',
   `update_time` datetime DEFAULT NULL COMMENT 'update time',
-  `cardnumber` varchar(11) DEFAULT NULL COMMENT 'card number',
+  `cardnumber` varchar(11) NOT NULL COMMENT 'card number',
   `email` varchar(40) DEFAULT NULL,
   `religion` varchar(20) DEFAULT NULL,
   `city` varchar(15) DEFAULT NULL,
   `state` varchar(15) DEFAULT NULL,
   `street` varchar(20) DEFAULT NULL,
   `zipcode` varchar(10) DEFAULT NULL,
-  `gender` int(1) NOT NULL DEFAULT 1 COMMENT 'gender'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='patient details table';
-
--- --------------------------------------------------------
+  `gender` int(1) NOT NULL DEFAULT 1 COMMENT 'gender',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `cardnumber` (`cardnumber`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8 COMMENT='patient details table';
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Stand-in structure for view `patient_view`
--- (See below for the actual view)
+-- Temporary table structure for view `patient_view`
 --
-CREATE TABLE `patient_view` (
-`id` int(11)
-,`cardnumber` varchar(11)
-,`firstname` varchar(20)
-,`lastname` varchar(20)
-,`phone` varchar(20)
-,`create_time` datetime
-,`update_time` datetime
-);
 
--- --------------------------------------------------------
+DROP TABLE IF EXISTS `patient_view`;
+/*!50001 DROP VIEW IF EXISTS `patient_view`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE TABLE `patient_view` (
+  `id` tinyint NOT NULL,
+  `cardnumber` tinyint NOT NULL,
+  `firstname` tinyint NOT NULL,
+  `lastname` tinyint NOT NULL,
+  `phone` tinyint NOT NULL,
+  `create_time` tinyint NOT NULL,
+  `update_time` tinyint NOT NULL
+) ENGINE=MyISAM */;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `patients`
+--
+
+DROP TABLE IF EXISTS `patients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `patients` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+  `cardnumber` varchar(11) NOT NULL COMMENT 'card number',
+  `firstname` varchar(20) NOT NULL,
+  `lastname` varchar(20) NOT NULL,
+  `phone` varchar(20) DEFAULT NULL COMMENT 'phone number',
+  `create_time` datetime DEFAULT current_timestamp() COMMENT 'create time',
+  `update_time` datetime DEFAULT NULL COMMENT 'update time',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `cardnumber` (`cardnumber`)
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `staff`
 --
 
+DROP TABLE IF EXISTS `staff`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `staff` (
-  `id` int(3) NOT NULL,
+  `id` int(3) NOT NULL AUTO_INCREMENT,
   `username` varchar(20) NOT NULL,
   `password` varchar(50) NOT NULL,
   `firstname` varchar(20) NOT NULL,
@@ -89,101 +113,55 @@ CREATE TABLE `staff` (
   `email` varchar(40) DEFAULT NULL,
   `createdOn` datetime DEFAULT current_timestamp(),
   `officeid` varchar(3) NOT NULL,
-  `post` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `staff`
---
-
-INSERT INTO `staff` (`id`, `username`, `password`, `firstname`, `lastname`, `phone`, `email`, `createdOn`, `officeid`, `post`) VALUES
-(1, 'boluwatife', '716b37cbb85ebeaea079d47aae047a3aed6aaa92', 'Boluwatife', 'Ade-Ojo', '08103490675', 'adeojopeter@gmail.com', '2021-08-09 15:47:26', 'ict', 'IT Officer'),
-(2, 'ipadeojo', '44a0a65bb2f51e6f86ac1b0f254ac6a3a8188d09', 'Idowu', 'Ade-Ojo', '08033886173', 'ipadeojo@eksu.edu.ng', '2021-08-09 16:32:47', 'cmd', 'Chief Medical Officer'),
-(3, 'grace', '716b37cbb85ebeaea079d47aae047a3aed6aaa92', 'Grace', 'Smith', '08035796823', 'gracesmith@maternalchildhosp.com', '2021-08-09 19:53:33', 'rec', 'Records Officer');
-
--- --------------------------------------------------------
+  `post` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `treatments`
 --
 
+DROP TABLE IF EXISTS `treatments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `treatments` (
-  `id` int(11) NOT NULL COMMENT 'primary key',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'primary key',
   `create_time` datetime DEFAULT current_timestamp() COMMENT 'create time',
   `update_time` datetime DEFAULT NULL COMMENT 'update time',
   `cardnumber` varchar(11) NOT NULL,
-  `file` varchar(255) NOT NULL
+  `file` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Structure for view `patient_view`
---
-DROP TABLE IF EXISTS `patient_view`;
-
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `patient_view`  AS   (select `patients`.`id` AS `id`,`patients`.`cardnumber` AS `cardnumber`,`patients`.`firstname` AS `firstname`,`patients`.`lastname` AS `lastname`,`patients`.`phone` AS `phone`,`patients`.`create_time` AS `create_time`,`patients`.`update_time` AS `update_time` from `patients`)  ;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Indexes for dumped tables
+-- Final view structure for view `patient_view`
 --
 
---
--- Indexes for table `patients`
---
-ALTER TABLE `patients`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `cardnumber` (`cardnumber`);
+/*!50001 DROP TABLE IF EXISTS `patient_view`*/;
+/*!50001 DROP VIEW IF EXISTS `patient_view`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_unicode_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `patient_view` AS (select `patients`.`id` AS `id`,`patients`.`cardnumber` AS `cardnumber`,`patients`.`firstname` AS `firstname`,`patients`.`lastname` AS `lastname`,`patients`.`phone` AS `phone`,`patients`.`create_time` AS `create_time`,`patients`.`update_time` AS `update_time` from `patients`) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for table `patient_details`
---
-ALTER TABLE `patient_details`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `cardnumber` (`cardnumber`);
-
---
--- Indexes for table `staff`
---
-ALTER TABLE `staff`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`);
-
---
--- Indexes for table `treatments`
---
-ALTER TABLE `treatments`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `patients`
---
-ALTER TABLE `patients`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'primary key';
-
---
--- AUTO_INCREMENT for table `patient_details`
---
-ALTER TABLE `patient_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'primary key';
-
---
--- AUTO_INCREMENT for table `staff`
---
-ALTER TABLE `staff`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `treatments`
---
-ALTER TABLE `treatments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'primary key';
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-08-10 10:48:34
